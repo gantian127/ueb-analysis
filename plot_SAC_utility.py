3275c0df0a97d2943d0c25eedf8ec134325099e5
gantian127 <jamy127@foxmail.com>
2017-10-19 19:40:58 -0600

    update for model comparison analysis code

--- plot_SAC_utility.py
+++ plot_SAC_utility.py
@@ -6,24 +6,25 @@ import matplotlib.dates as mdates
 import pandas as pd
 import math
 from datetime import datetime, timedelta
+import numpy as np
+import calendar
+import os
 
 
-def plot_multiple_time_series(time, data_list, label_list,
-                              fig=None, ax=None,
-                              title=None,
-                              xlabel=None,
-                              ylaebl=None,
-                              xlim=None,
-                              ylim=None,
-                              interval=1,
-                              format='%Y/%m'
+def plot_multiple_X_Y(time, data_list,
+                      label_list=None,
+                      fig=None, ax=None,
+                      xlim=None,
+                      ylim=None,
+                      xticks=False,
+                      figsize=None,
                               ):
 
     if ax is None:
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=figsize)
 
     for i in range(0, len(data_list)):
-        ax.plot(time, data_list[i], label=label_list[i])
+        ax.plot(time, data_list[i], label=label_list[i] if label_list else str(i))
 
     if xlim:
         ax.set_xlim(xlim[0], xlim[1])
@@ -31,10 +32,13 @@ def plot_multiple_time_series(time, data_list, label_list,
     if ylim:
         ax.set_ylim(ylim[0], ylim[1])
 
+    if xticks:
+        ax.set_xticks(time)
+
     return fig, ax
 
 
-def refine_plot(ax, xlabel='', ylabel='', title='', interval=1, format='%Y/%m', legend=True, time_axis=True):
+def refine_plot(ax, xlabel='', ylabel='', title='', interval=1, format='%Y/%m', text=None, text_position=None, legend=True, time_axis=True):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_title(title)
@@ -45,6 +49,9 @@ def refine_plot(ax, xlabel='', ylabel='', title='', interval=1, format='%Y/%m', 
 
     if legend:
         ax.legend(loc='best')
+
+    if text and text_position:
+        plt.text(text_position[0], text_position[1], text)
 
 
 def save_fig(fig, save_as=None):
@@ -60,6 +67,9 @@ def get_sacsma_time_and_discharge(discharge=None, surf_flow=None, sub_flow=None,
                           discharge_skip=91, surf_flow_skip=62, sub_flow_skip=62,
                           unit_factor=1, unit_offset=0,
                           time_hr_change='24', time_hr_change_factor=1):
+    """
+    used for auto calibration option selection
+    """
     results = {}
 
     if discharge:
@@ -73,7 +83,7 @@ def get_sacsma_time_and_discharge(discharge=None, surf_flow=None, sub_flow=None,
         time_obj = []
         for x in time_str:
             if x[-2:] == time_hr_change:
-                x = x[:-2] + x[-2:].replace('24', '23')
+                x = x[:-2] + x[-2:].replace(time_hr_change, '23')
                 time = datetime.strptime(x, '%d%m%y%H')
                 time += timedelta(hours=time_hr_change_factor)
             else:
@@ -119,6 +129,9 @@ def get_sacsma_time_and_discharge(discharge=None, surf_flow=None, sub_flow=None,
 
 
 def get_data_by_time_aggregation(time, data, freq='D'):
+    """
+    used for auto calibration option selection
+    """
     df = pd.DataFrame(data={'time': time, 'data': data}, columns=['time', 'data'])
     data = df.set_index('time').groupby(pd.TimeGrouper(freq=freq))['data'].mean()
 
@@ -127,6 +140,10 @@ def get_data_by_time_aggregation(time, data, freq='D'):
 
 def get_rit_discharge(discharge, skiprows=3, start_date_obj=None, end_date_obj=None,
                       unit_factor=0.0283168, unit_offset=0):
+    """
+    used for auto calibration option selection
+    """
+
     rti_discharge = pd.read_csv(discharge, skiprows=skiprows, header=None, names=['raw'])  # time column is used as index in dataframe
     if start_date_obj and end_date_obj:
         start_date = datetime.strftime(start_date_obj, '%Y-%m-%d')
@@ -136,6 +153,38 @@ def get_rit_discharge(discharge, skiprows=3, start_date_obj=None, end_date_obj=N
         discharge = rti_discharge['raw'].apply(lambda x: x * unit_factor + unit_offset).tolist()
 
     return discharge
+
+
+# Functions used for 22yr model comparision and data analysis
+def get_sim_obs_dataframe(sim_file, obs_file, start_time='', end_time='', sim_skip=91, obs_skip=3, obs_unit=0.0283168):
+    """
+     This will create the daily sim vs obs data frame for discharge comparision
+    """
+    # get sim daily data
+    raw_sim = pd.read_csv(sim_file, skiprows=sim_skip, header=None, names=['raw'])
+    sim_data = raw_sim['raw'].str.split('\s+', expand=True)
+    sim_data.rename(columns={3: 'sim'}, inplace=True)
+    sim_data['sim'] = sim_data['sim'].astype(float)
+    sim_data[[2]] = sim_data[[2]].apply(lambda x: x.replace('25', '1'))
+    sim_data['time'] = sim_data[[1, 2]].apply(lambda x: ''.join(x), axis=1)
+    sim_data['time'] = pd.to_datetime(sim_data['time'], format='%d%m%y%H')
+    sim_data.drop([0, 1, 2], axis=1, inplace=True)
+    sim_data.ix[sim_data.sim < 0, 'sim'] = np.nan
+    sim = sim_data.set_index('time').groupby(pd.TimeGrouper(freq='D'))['sim'].mean()
+
+    if start_time and end_time:
+        sim = sim[(sim.index >= start_time) & (sim.index <= end_time)]
+
+    # get observation data
+    obs = pd.read_csv(obs_file, skiprows=obs_skip, header=None, names=['obs'])
+    obs.index = pd.to_datetime(obs.index, format='%Y-%m-%d %H:%M:%S')
+    obs['obs'] = obs['obs'].apply(lambda x: x*obs_unit)
+    obs.ix[obs.obs < 0, 'obs'] = np.nan  # remove negative value
+
+    # create dataframe
+    DF = pd.concat([sim, obs], axis=1, join_axes=[sim.index]).reset_index()
+
+    return DF
 
 
 def get_statistics(sim, obs):
@@ -170,8 +219,11 @@ def get_statistics(sim, obs):
     return {'rmse': rmse, 'nse': nse, 'mae': mae, 'r': r}
 
 
-def plot_obs_vs_sim(time, sim, obs,  figsize=(15,10),
-                    ts_title='Time series of observation and simulation discharge',
+def plot_obs_vs_sim(
+                    time=None, sim=None, obs=None,
+                    DF=None,
+                    figsize=[(15,10),(15,5)],
+                    ts_title='Time series of observation vs. simulation discharge',
                     ts_xlabel='Time',
                     ts_ylabel='Discharge(cms)',
                     ts_xlim=None,
@@ -181,47 +233,25 @@ def plot_obs_vs_sim(time, sim, obs,  figsize=(15,10),
                     ylabel='Simulation(cms)',
                     xlim=None,
                     ylim=None,
-                    text_position=[0.1, 0.90],
+                    text_position=[0.1, 0.80],
                     month_interval=1,
                     format='%Y',
-                    save_as=False
+                    save_folder=None,
                     ):
 
     # calculate statistics
-    stat_df = pd.DataFrame({'time': time,
-                            'discharge': sim,
-                            'observation': obs},
-                             columns=['time', 'discharge', 'observation'])
-    #
-    # # rmse: sqrt(mean(sum((Qs-Qo)**2)))
-    # stat_df['valDiff'] = stat_df['observation'] - stat_df['discharge']
-    # valDiff_mean = stat_df['valDiff'].mean()
-    # stat_df['valDiffSq'] = stat_df['valDiff'].apply(lambda x: x**2)
-    # valDiffSq_mean = stat_df['valDiffSq'].mean()
-    # rmse = math.sqrt(valDiffSq_mean)
-    #
-    # # nse: 1 - sum ((Qs-Qo)**2) / sum((Qo-Qomean)**2)
-    # stat_df['valDiffA'] = stat_df['valDiff'].apply(lambda x: abs(x))
-    # valDiffA_mean = stat_df['valDiffA'].mean()
-    # obs_mean = stat_df['observation'].mean()
-    # valDiffSq_sum = stat_df['valDiffSq'].sum()
-    #
-    # stat_df['valDiffMean'] = stat_df['observation'].apply(lambda x: x - obs_mean)
-    # stat_df['valDiffSqmean'] = stat_df['valDiffMean'].apply(lambda x: x**2)
-    # valDiffSqmean_sum = stat_df['valDiffSqmean'].sum()
-    #
-    # nse = 1 - (valDiffSq_sum/valDiffSqmean_sum)
-    # mae = valDiffA_mean
-    #
-    # # correlation coefficient
-    # r = stat_df[['observation', 'discharge']].corr()['observation'][1]
+    if DF is None:
+        DF = pd.DataFrame({'time': time,
+                            'sim': sim,
+                            'obs': obs},
+                            columns=['time', 'sim', 'obs'])
 
-    statistics = get_statistics(stat_df['discharge'], stat_df['observation'])
+    statistics = get_statistics(DF['sim'], DF['obs'])
 
     # plot obs vs sac time series
-    fig, ax = plt.subplots(2, 1, figsize=figsize)
-    plot_multiple_time_series(stat_df['time'],
-                              [stat_df['observation'], stat_df['discharge']],
+    fig, ax = plt.subplots(2, 1, figsize=figsize[0])
+    plot_multiple_X_Y(DF['time'],
+                              [DF['obs'], DF['sim']],
                               ['observation', 'simulation'],
                               ax=ax[0], fig=fig,
                               xlim=ts_xlim,
@@ -231,11 +261,11 @@ def plot_obs_vs_sim(time, sim, obs,  figsize=(15,10),
                 legend=True, interval=month_interval, format=format)
 
     # plot obs vs sac scatter plot
-    ax[1].scatter(stat_df['observation'], stat_df['discharge'])
+    ax[1].scatter(DF['obs'], DF['sim'])
     refine_plot(ax[1], xlabel=xlabel, ylabel=ylabel, title=title,
                 time_axis=False, legend=False)
-    ax[1].plot([stat_df['observation'].min(), stat_df['observation'].max()],
-                [stat_df['observation'].min(), stat_df['observation'].max()],
+    ax[1].plot([DF['obs'].min(), DF['obs'].max()],
+                [DF['obs'].min(), DF['obs'].max()],
                 linestyle = ':',
                 color='r',
                 )
@@ -245,14 +275,120 @@ def plot_obs_vs_sim(time, sim, obs,  figsize=(15,10),
     if ylim:
         ax[1].set_ylim(ylim[0], ylim[1])
 
-    plt.text(stat_df['observation'].max()*text_position[0], stat_df['discharge'].max()*text_position[1],
+    plt.text(DF['obs'].max()*text_position[0], DF['sim'].max()*text_position[1],
              ' RMSE = {} \n NSE = {} \n R = {} \n MAE = {}'.format(statistics['rmse'], statistics['nse'],
                                                                    statistics['r'], statistics['mae']),
              fontsize=11, color='b')
 
     plt.tight_layout()
 
-    if save_as:
-        save_fig(fig, save_as=save_as)
+    if save_folder:
+        save_fig(fig, save_as=os.path.join(save_folder,'time_serise.png'))
+
+    # plot daily bias
+    DF['bias'] = DF['sim'] - DF['obs']
+    fig, ax = plot_multiple_X_Y(DF['time'], [DF['bias']],
+                                xlim=ts_xlim,
+                                figsize=figsize[1],
+                                label_list=['sim-obs'])
+    refine_plot(ax, xlabel='time', ylabel='discharge(cms)',
+                title='Daily mean bias',
+                time_axis=True,
+                interval=month_interval,
+                format=format,
+                legend=True,
+                )
+    if save_folder:
+        save_fig(fig, save_as=os.path.join(save_folder,'time_series_bias.png'))
 
     return fig, ax
+
+
+def get_monthly_mean_analysis(DF, watershed_area, save_folder=None):
+    # monthly_mean
+    monthly_mean = DF.groupby([DF.time.dt.month, DF.time.dt.year]).mean()
+    monthly_mean_multiyear = monthly_mean.groupby(level=0).mean()
+    monthly_mean_multiyear['bias'] = monthly_mean_multiyear['sim'] - monthly_mean_multiyear['obs']
+    bias_mean = monthly_mean_multiyear['bias'].mean()
+    percent_bias = sum(monthly_mean_multiyear['bias']) / sum(monthly_mean_multiyear['obs']) * 100
+
+    monthly_mean_multiyear['factor'] = [calendar.monthrange(2010, x)[1] * 24 * 3600 * (watershed_area ** -1) * 100 for x
+                                        in range(1, 13)]  # depth in mm
+    monthly_mean_multiyear['sim_depth'] = monthly_mean_multiyear.sim * monthly_mean_multiyear.factor
+    monthly_mean_multiyear['obs_depth'] = monthly_mean_multiyear.obs * monthly_mean_multiyear.factor
+    monthly_mean_multiyear['bias_depth'] = monthly_mean_multiyear['sim_depth'] - monthly_mean_multiyear['obs_depth']
+    bias_mean_depth = monthly_mean_multiyear['bias_depth'].mean()
+    percent_bias_depth = sum(monthly_mean_multiyear['bias_depth']) / sum(monthly_mean_multiyear['obs_depth']) * 100
+
+    # write results
+    path = os.path.join(save_folder, 'monthly_mean_multiyear.csv') if save_folder else './monthly_mean_multiyear.csv'
+    monthly_mean_multiyear.to_csv(path)
+    with open(path, 'a') as my_file:
+        my_file.write(
+            '\nbias mean = {}'
+            '\npercent bias = {}'
+            '\nbias mean depth = {}'
+            '\npercent bias depth = {}'.format(bias_mean, percent_bias, bias_mean_depth, percent_bias_depth)
+        )
+
+    # make plots
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(2, 1, figsize=(15, 10))
+    plot_multiple_X_Y(monthly_mean_multiyear.index,
+                      [monthly_mean_multiyear.sim, monthly_mean_multiyear.obs],
+                      ax=ax[0], fig=fig,
+                      label_list=['sim', 'obs'],
+                      xticks=True)
+
+    refine_plot(ax[0], xlabel='month', ylabel='discharge(cms)',
+                title='Monthly mean discharge',
+                legend=True,
+                time_axis=False)
+
+    plot_multiple_X_Y(monthly_mean_multiyear.index,
+                      [monthly_mean_multiyear.bias],
+                      ax=ax[1], fig=fig,
+                      label_list=['bias'])
+
+    refine_plot(ax[1], xlabel='month', ylabel='bias(cms)',
+                title='Monthly mean bias',
+                legend=True,
+                time_axis=False,
+                text='mean bias = {} \npercent bias= {}'.format(bias_mean, percent_bias),
+                text_position=[1, -6]
+                )
+
+    path = os.path.join(save_folder, 'monthly_mean_bias.png') if save_folder else 'monthly_mean_bias.png'
+    save_fig(fig, path)
+
+    fig, ax = plt.subplots(2, 1, figsize=(15, 10))
+    plot_multiple_X_Y(monthly_mean_multiyear.index,
+                      [monthly_mean_multiyear.sim_depth, monthly_mean_multiyear.obs_depth],
+                      ax=ax[0], fig=fig,
+                      xticks=True,
+                      label_list=['sim', 'obs'])
+
+    refine_plot(ax[0], xlabel='month', ylabel='discharge(mm)',
+                title='Monthly mean discharge (in depth)',
+                legend=True,
+                time_axis=False)
+
+    plot_multiple_X_Y(monthly_mean_multiyear.index,
+                      [monthly_mean_multiyear.bias_depth],
+                      ax=ax[1], fig=fig,
+                      label_list=['bias'],
+                      xticks=True)
+
+    refine_plot(ax[1], xlabel='month', ylabel='bias(mm)',
+                title='Monthly mean bias (in depth)',
+                legend=True,
+                time_axis=False,
+                text='mean bias = {} \npercent bias= {}'.format(bias_mean_depth, percent_bias_depth),
+                text_position=[1, -1]
+                )
+
+    path = os.path.join(save_folder, 'monthly_mean_bias_depth.png') if save_folder else 'monthly_mean_bias_depth.png'
+    save_fig(fig, path)
+
+    return 'monthly mean analysis done'
