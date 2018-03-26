85c7533322735aa32cadce4203f919f222de0580
gantian127 <jamy127@foxmail.com>
2018-03-26 14:35:10 -0600

    update auto_cali analysis for DOLC2

--- 22yr_SAC_auto_cali_results_2.py
+++ 22yr_SAC_auto_cali_results_2.py
@@ -2,10 +2,21 @@
 This is used to analyze all the auto-calibration population results.
 This is to replace version 1. Only need to run this script to get the analysis and results
 
-- time series plots for all populations and the best option (least percent error sum)
-- box plot to show the parameter values and the objective values
+requirements:
+- put this code and the utility code at the same level with the test folder
 
+steps:
+- analyze the time serise:
+  a) time series plots for all populations and the best option (least percent error sum)
+  b) box plot to show the parameter values
+  c) box plot to show the stats values for discharge time series
 
+- analyze the front and decision result:
+  a) box plot of front values (objective function values)
+  b) box plot of decision values (parameter values)
+
+- analyze the peadj parameters:
+  a) box plot for peadj values
 
 """
 
@@ -19,23 +30,31 @@ import calendar
 import linecache
 
 
-# user settings
-watershed_area = 1818920000
-obs_file_path = '/uufs/chpc.utah.edu/common/home/ci-water6-1/TianGan/22yr_Animas_watershed_sac/Paul_test_2/data/hisobs/DRGC2H_F.QME'
+# user settings ####################################################
+watershed_name = 'DOLC2'
+watershed_area = 1503860000
+obs_file_path = '/uufs/chpc.utah.edu/common/home/ci-water6-1/TianGan/22yr_Animas_watershed_sac/Paul_test_2/data/hisobs/DOLC2L_F.QME'
 output_folders = [
-                  'test_work2_1st',
-                  'test_work2_2nd_for_plot',
+                  # 'test_work2_1st',
+                  # 'test_work2_2nd_for_plot',
+                    'test_DOLC2_1st',
+                    'test_DOLC2_2nd'
                  ]
 start_time = '1989/10/01'
-end_time = '1999/06/30'
+end_time = '2010/09/30'
 
 
 output2_folders = [
-    '/uufs/chpc.utah.edu/common/home/ci-water6-1/TianGan/22yr_Animas_watershed_sac/Paul_test_2/UEB_DRGC2_NSGAII_Parallel_RDHM_NASA_work2_for_plot/',
+    # '/uufs/chpc.utah.edu/common/home/ci-water6-1/TianGan/22yr_Animas_watershed_sac/Paul_test_2/UEB_DRGC2_NSGAII_Parallel_RDHM_NASA_work2_for_plot/',
+    '/uufs/chpc.utah.edu/common/home/ci-water6-1/TianGan/22yr_Animas_watershed_sac/Paul_test_2/UEB_DRGC2_NSGAII_Parallel_RDHM_NASA/',
     ]
 
+result_dir = os.path.join(os.getcwd(), 'auto_cali_analysis_{}'.format(watershed_name))
+if not os.path.isdir(result_dir):
+    os.mkdir(result_dir)
 
-# # Time series analysis
+# Time series analysis #######################################################
+print 'step1: time series analysis'
 for i in range(0, len(output_folders)):
 
     # get option and observation time series
@@ -81,7 +100,7 @@ for i in range(0, len(output_folders)):
     best_vol_err = stat_result['vol_err'].abs().idxmin()
     DF2 = pd.concat([DF[best_folder], obs], axis=1, join='inner')
 
-    path = './stat_result_{}.csv'.format(output_folder)
+    path = os.path.join(result_dir, 'stat_result_{}.csv'.format(output_folder))
     stat_result.to_csv(path)
     with open(path, 'a') as my_file:
         my_file.write(
@@ -110,7 +129,7 @@ for i in range(0, len(output_folders)):
     a = stat_result.loc[best_folder]
     text = 'rmse = {} \nnse= {} \nr= {} \nbias= {} \n '.format(a['rmse'], a['nse'], a['r'], a['bias'])
     ax.text(0.05, 0.8, text, transform=ax.transAxes)
-    fig.savefig('time_series_band_plot_{}.png'.format(output_folder), dpi=1200)
+    fig.savefig(os.path.join(result_dir, 'time_series_band_plot_{}.png'.format(output_folder)))
 
     # plot the boxplot for percent bias
     fig2, ax2 = plt.subplots()
@@ -118,7 +137,7 @@ for i in range(0, len(output_folders)):
     ax2.set_ylabel('percent error (%)')
     ax2.set_title('Boxplot of different percent bias')
     ax2.set_xticklabels(['annual mean bias', 'monthly mean bias', 'April-July volume error'])
-    fig2.savefig('boxplot_of_percent_bias_{}.png'.format(output_folder), dpi=1200)
+    fig2.savefig(os.path.join(result_dir, 'boxplot_of_percent_bias_{}.png'.format(output_folder)))
 
     # plot the boxplot for stats
     fig3, ax3 = plt.subplots(2, 2, figsize=(10,8))
@@ -129,12 +148,11 @@ for i in range(0, len(output_folders)):
     plt.suptitle('Boxplot of basic stats')
     ax3[1, 0].set_ylabel('discharge(cms)')
     ax3[1, 1].set_ylabel('discharge(cms)')
-    fig3.savefig('boxplot_of_basic_stats_{}.png'.format(output_folder), dpi=1200)
-
-print 'Time series plot finished'
+    fig3.savefig(os.path.join(result_dir, 'boxplot_of_basic_stats_{}.png'.format(output_folder)))
 
 
-# decisions000.dat and front000.dat analysis
+# decisions000.dat and front000.dat analysis  #####################################################
+print 'step2: decision and front analysis'
 for i in range(0, len(output2_folders)):
     output2_folder = output2_folders[i]
     decision_file_path = os.path.join(output2_folder, 'Decision000.dat')
@@ -147,7 +165,7 @@ for i in range(0, len(output2_folders)):
         ax4.set_xticklabels(['sac_UZTWM','sac_UZFWM','sac_LZTWM','sac_LZFPM','sac_LZFSM','sac_UZK','sac_LZPK',
                              'sac_LZSK','sac_ZPERC','sac_REXP','sac_PFREE','Beta0','Beta1','Beta2',], fontdict={'fontsize':7}) # this is got from 'GeneHeader.txt'
         plt.suptitle('Boxplot for Decision000.dat file')
-        fig4.savefig('decision000_{}.png'.format(i), dpi=1200)
+        fig4.savefig(os.path.join(result_dir, 'decision000_{}.png'.format(i)))
 
     if os.path.isfile(front_file_path):
         front_df = pd.read_csv(front_file_path, header=None, sep='\s+')
@@ -157,12 +175,12 @@ for i in range(0, len(output2_folders)):
         front_df.boxplot(['volume difference'], grid=False, ax=ax5[1])
         front_df.boxplot(['penalty'], grid=False, ax=ax5[2])
         plt.suptitle('Boxplot for front000.dat file')
-        fig5.savefig('front000_{}.png'.format(i), dpi=1200)
-
-print 'front.dat and decision.dat analysis is done'
+        fig5.savefig(os.path.join(result_dir, 'front000_{}.png'.format(i)))
 
 
-# peadj parameter analysis
+
+# peadj parameter analysis #########################################################################
+print 'step3: peadj analysis'
 for i in range(0, len(output_folders)):
 
     output_folder = output_folders[i]
@@ -190,10 +208,10 @@ for i in range(0, len(output_folders)):
     # boxplot for peadj values
     fix6, ax6 = plt.subplots(figsize=(15, 5))
     peadj_result.boxplot(grid=False, ax=ax6)
-    fix6.savefig('peadj_{}.png'.format(output_folder), dpi=1200)
+    fix6.savefig(os.path.join(result_dir, 'peadj_{}.png'.format(output_folder)))
 
     # save results
-    path = './peadj_result_{}.csv'.format(output_folder)
+    path = os.path.join(result_dir, 'peadj_result_{}.csv'.format(output_folder))
     peadj_result.to_csv(path)
 
-print 'peadj analysis is done'
+print 'All analysis is done'
