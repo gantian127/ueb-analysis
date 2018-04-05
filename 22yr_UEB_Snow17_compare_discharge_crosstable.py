eef336a9e8a999c53ce356097e31b0241192fa55
gantian127 <jamy127@foxmail.com>
2018-04-05 17:55:31 -0600

    add PSS score for contingency table calculation

--- 22yr_UEB_Snow17_compare_discharge_crosstable.py
+++ 22yr_UEB_Snow17_compare_discharge_crosstable.py
@@ -3,6 +3,7 @@ This is used to compare the snow17 workflow, ueb workflow, observation discharge
 (2015 Magnusson) Evaluating snow models with varying process representations for hydrological applications
 (1992 Gerrity) A note on Gandin and Murphy's Equitable Skill score
 Gerrity score function: http://www.cawcr.gov.au/projects/verification/#Methods_for_foreasts_of_continuous_variables
+Hanssen and Kuipers (Peirce's skill score) http://www.cawcr.gov.au/projects/verification/
 
 results:
 - Dataframe: sort the values and calcualte the percentile, assign discharge in different level dry, low, medium, high
@@ -25,9 +26,9 @@ from plot_SAC_utility import get_obs_dataframe,get_sim_dataframe
 watershed = 'DOLC2'
 watershed_area = 0
 
-snow17_file = r'D:\Research_Data\Mcphee_DOLC2\snow17_best_time_series\DOLC2_discharge_outlet.ts'
-ueb_file = r'D:\Research_Data\Mcphee_DOLC2\UEB_best_time_series\DOLC2_discharge_outlet.ts'
 obs_file = r'D:\Research_Data\Mcphee_DOLC2\DOLC2L_F.QME'
+ueb_file = r'D:\Research_Data\Mcphee_DOLC2\UEB_best_time_series\DOLC2_discharge_outlet.ts'
+snow17_file = r'D:\Research_Data\Mcphee_DOLC2\snow17_best_time_series\DOLC2_discharge_outlet.ts'
 
 snow17_skip = 136
 ueb_skip = 121
@@ -46,7 +47,7 @@ obs_df = get_obs_dataframe(obs_file, start_time, end_time)
 ueb_df = get_sim_dataframe(ueb_file, start_time=start_time, end_time=end_time, sim_skip=ueb_skip)
 snow17_df = get_sim_dataframe(snow17_file, start_time=start_time, end_time=end_time, sim_skip=snow17_skip)
 
-DF = pd.concat([ueb_df, snow17_df, obs_df], axis=1)
+DF = pd.concat([obs_df, ueb_df, snow17_df], axis=1)  # obs should be first used by the loop to get the discharge threshold
 DF.columns = ['obs', 'ueb', 'snow17']  # obs should be first used by the loop to get the discharge threshold
 percentile_list = [0, 0.75, 0.90, 0.99, 1]
 level_list = ['dry', 'low', 'medium', 'high']
@@ -95,6 +96,7 @@ with open(os.path.join(result_dir, 'discharge_threshold.csv'), 'w') as f:
 
 # step 3 make the contingency table and Gerrity score #########################################
 GS_result = []
+PSS_result = []
 for name in ['snow17', 'ueb']:
     # contingency table
     table_df = pd.DataFrame(columns=level_list, index=level_list)
@@ -135,9 +137,36 @@ for name in ['snow17', 'ueb']:
     GS = 1./N * (S_matrix*N_matrix.values).sum().sum()
     GS_result.append([name, GS])
 
-cal_df.to_csv(os.path.join(result_dir, 'cal_df.csv'))
-S_matrix.to_csv(os.path.join(result_dir, 'S_matrix.csv'))
+    mul_df = S_matrix*N_matrix.values
+    mul_df['total_score'] = mul_df.sum(axis=1)
+    mul_df.ix['total_score'] = mul_df.sum(axis=0)
+    mul_df.to_csv(os.path.join(result_dir,'multiply_matrix_{}.csv'.format(name)))
+
+    # Pierce score
+    K = len(level_list)
+    N = table_df.ix['total'].sum()
+    cal_df = pd.DataFrame()
+    P_matrix = table_df
+    P_matrix['total'] = table_df.sum(axis=1)
+    P_matrix = P_matrix / N
+    cal_df['pii'] = [P_matrix.ix[i, i] for i in range(0, K)]
+    cal_df['pi'] = [P_matrix.ix['total', i] for i in range(0, K)]
+    cal_df['qi'] = [P_matrix['total'].ix[i] for i in range(0, K)]
+    cal_df['piqi'] = cal_df['pi'] * cal_df['qi']
+    cal_df['pipi'] = cal_df['pi'] ** 2
+    PSS = (cal_df['pii'].sum() - cal_df['piqi'].sum()) / (1 - cal_df['pipi'].sum())
+    PSS_result.append([name, PSS])
+
+    cal_df.to_csv(os.path.join(result_dir, 'PSS_cal_df_{}.csv'.format(name)))
+
 
 with open(os.path.join(result_dir, 'GS_result.csv'), 'w') as f:
     writer = csv.writer(f)
     writer.writerows(GS_result)
+
+with open(os.path.join(result_dir, 'PSS_result.csv'), 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(PSS_result)
+
+cal_df.to_csv(os.path.join(result_dir, 'cal_df.csv'))
+S_matrix.to_csv(os.path.join(result_dir, 'S_matrix.csv'))
